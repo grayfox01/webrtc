cd515ef12f96edcbaa5715eb1cc8907b9a3cd788
Kevin Hincapie <kvn.hincapie.t@gmail.com>
2017-06-28 19:55:55 -0500

    asdasdsd

--- server3.js
+++ server3.js
@@ -1,6 +1,4 @@
 var http = require('http');
-var https = require('https');
-var fs = require('fs');
 var express = require("express");
 
 var app = express();
@@ -8,17 +6,10 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(request, response) {
+  console.log('[support dash] processing get request')
   response.send('Hello World 2!');
 });
 
-var privateKey = fs.readFileSync(__dirname + '/ssl/server.key').toString();
-var certificate = fs.readFileSync(__dirname + '/ssl/gandiSSL.pem').toString();
-
-var options = {
-  key: privateKey,
-  cert: certificate
-};
-
-https.createServer(options, app).listen(process.env.PORT, function () {
-  console.log("Express server listening on port " + app.get('port'));
+app.listen(process.env.PORT, function () {
+  console.log('***** exp listening on port: ' + process.env.PORT);
 });
